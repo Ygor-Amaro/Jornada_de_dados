ccb9c3ca09a2c091c4989b46d0a93bbafa729b6c
Ygor-Amaro <yka09@gmail.com>
2026-03-28 16:05:52 -0300

    update

--- Bootcamp_SQL_dbt/aula_03/exercicios_join.sql
+++ Bootcamp_SQL_dbt/aula_03/exercicios_join.sql
@@ -32,7 +32,39 @@ WHERE orders_by_customer = 0;
 
 -- 2 - The Management TreeShow the name of every Employee and the name of their Manager.
 
+SELECT 
+	m.first_name AS manager,
+	e.first_name AS employee,
+	e.title AS job_role
+FROM employees AS e
+LEFT JOIN employees AS m ON e.reports_to = m.employee_id
+ORDER BY manager
+
 -- 3 - The Revenue ReporterFind the total revenue (Price × Quantity) per Category in the year 1997.categories, products, order_details, orders.
+
+WITH revenue_per_product AS (
+    SELECT 
+        d.product_id,
+        SUM(d.unit_price * d.quantity) AS total_revenue
+    FROM order_details d
+    INNER JOIN orders o ON d.order_id = o.order_id
+    WHERE o.order_date >= '1997-01-01' AND o.order_date < '1998-01-01'
+    GROUP BY d.product_id
+),
+category_mapping AS (
+    SELECT 
+        p.product_id,
+        c.category_name
+    FROM products p
+    INNER JOIN categories c ON p.category_id = c.category_id
+)
+SELECT 
+    m.category_name,
+    SUM(r.total_revenue) AS category_revenue
+FROM category_mapping AS m
+INNER JOIN revenue_per_product AS r ON m.product_id = r.product_id
+GROUP BY m.category_name
+ORDER BY category_revenue DESC;
 
 -- Section 2: Focus on Window Functions (The Magic Glass).
 -- 4 - Cumulative RevenueShow each order's freight cost and a running total of freight paid by that specific customer.SUM(freight) OVER (PARTITION BY customer_id ORDER BY order_date).
